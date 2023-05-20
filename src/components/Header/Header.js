7186259946a1103e55cd072e9ddbb1c72ea7a8de
Felix A. Schultz <felix.schultz@engapho.com>
2023-05-20 18:25:32 +0200

    Setup local items

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,15 +1,24 @@
 const { useState, useEffect, useRef } = React;
+const Link = window.ReactRouterDOM.Link;
+import { local } from "../../localization/local";
+
 import "./Header.css";
+
+/* localStorage.setItem("region", "da"); */
+
+const region = (localStorage.getItem("region")===null) ? "da" : localStorage.getItem("region");
+
 export default function Header(){
+
     return (<>
         <header className="main-header">
             <section className="small-topHeader">
-                <div class="paymentMethods">
+                <div className="paymentMethods">
                 </div>
                 <a className="trustpilot-review" href="https://dk.trustpilot.com/evaluate/cykelfaergen.info" target="_blank">Bedøm os på <img src="https://cdn.trustpilot.net/brand-assets/1.8.0/logo-black.svg" alt="Trustpilot" /></a>
-                <div class="operator">
+                <div className="operator">
                     Booking via:
-                    <img class="operator-logo" src="https://www.cykelfaergen.info/assets/logo/als-turbaade.png" title="Als Turbåde" alt="Als Turbåde" />
+                    <img className="operator-logo" src="https://www.cykelfaergen.info/assets/logo/als-turbaade.png" title="Als Turbåde" alt="Als Turbåde" />
                 </div>
             </section>
             <section className="header">
@@ -20,9 +29,11 @@ export default function Header(){
                 </section>
                 <nav className="navigation">
                     <ul>
-                        <li className="lang">
-                            <a class="lang_chooser">Sejlplaner <i class="arrow right-arrow"></i></a>
-                        </li>
+                        {
+                            Object.keys(local[region].navigation).map((key, index) => {
+                                return <li key={index}><Link className="" to={local[region].navigation[key].link}>{local[region].navigation[key].name}</Link></li>
+                            })
+                        }
                     </ul>
                 </nav>
             </section>
---
+++ src/localization/localize.de.js
@@ -0,0 +1,28 @@
+export default {
+    navigation: {
+        timetable: {
+            name: "Fahrplan",
+            type: "dropdown",
+            link: ""
+        },
+        price: {
+            name: "Preis",
+            link: ""
+        },
+        specialTours: {
+            name: "Specail Tours",
+            type: "dropdown",
+            link: ""
+        },
+        services:{
+            name: "Radservice & Erkundung",
+            type: "dropdown",
+            link: ""
+        },
+        about: {
+            name: "Über uns",
+            type: "dropdown",
+            link: ""
+        }
+    }
+}
